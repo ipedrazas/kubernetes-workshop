b4efbbdf8f309ed2c4fb2b5d199d8c9cf6826070
Ivan Pedrazas <ipedrazas@gmail.com>
2016-01-17 15:29:18 +0000

    added mods from workshop

--- examples/ping-redis/main.go
+++ examples/ping-redis/main.go
@@ -1,53 +1,52 @@
 package main
 
 import (
-    "fmt"
-    "gopkg.in/redis.v3"
-    "log"
-    "net/http"
-    "os"
+	"fmt"
+	"gopkg.in/redis.v3"
+	"log"
+	"net/http"
+	"os"
 )
 
 var redisClient *Redis
 
 type Redis struct {
-    *redis.Client
+	*redis.Client
 }
 
 func (r Redis) getCount() (int64, error) {
-    if err := redisClient.Incr("ping").Err(); err != nil {
-        return -1, err
-    }
+	if err := redisClient.Incr("ping").Err(); err != nil {
+		return -1, err
+	}
 
-    count, err := redisClient.Get("ping").Int64()
-    if err != nil {
-        return -1, err
-    }
-    return count, err
+	count, err := redisClient.Get("ping").Int64()
+	if err != nil {
+		return -1, err
+	}
+	return count, err
 }
 
 func pingServer(w http.ResponseWriter, req *http.Request) {
-    var message string
+	var message string
 
-    if count, err := redisClient.getCount(); err == nil {
-        message = fmt.Sprintf("ping! %d", count)
-    } else {
-        message = err.Error()
-    }
-    w.Write([]byte(message))
+	if count, err := redisClient.getCount(); err == nil {
+		message = fmt.Sprintf("ping! %d", count)
+	} else {
+		message = err.Error()
+	}
+	w.Write([]byte(message))
 }
 
 func main() {
-    uri := fmt.Sprintf("%s:6379", os.Getenv("REDIS_HOST"))
-    redisClient = &Redis{
-        redis.NewClient(&redis.Options{
-            Addr:     uri,
-            Password: "", // no password set
-            DB:       0,  // use default DB
-        }),
-    }
+	uri := fmt.Sprintf("%s:6379", os.Getenv("REDIS_HOST"))
+	redisClient = &Redis{
+		redis.NewClient(&redis.Options{
+			Addr:     uri,
+			Password: "", // no password set
+			DB:       0,  // use default DB
+		}),
+	}
 
-    http.HandleFunc("/", pingServer)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", pingServer)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
